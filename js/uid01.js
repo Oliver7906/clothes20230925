e0ab3854745488a8dde4294238a8568aef59fcae
Oliver7906 <d21xz02@gmail.com>
2023-09-27 13:12:41 +0800

    20230927

--- js/uid01.js
+++ js/uid01.js
@@ -80,6 +80,7 @@ $(function () {
   function logout() {
     setCookie("uid01", "", 7);
     setCookie("level", " ", 7);
+    $("#out_toast_msg").text("已經登出了");
     location.href ="index.html";
   }
 });
